53c226756c28328cad546536ca2fce5a4d128af9
jenlau250 <jlau250@gmail.com>
2019-09-22 23:07:22 -0700

    Initial commit

---
+++ app/src/main/java/android/bignerdranch/com/beatbox/BeatBoxActivity.java
@@ -0,0 +1,14 @@
+package android.bignerdranch.com.beatbox;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+public class BeatBoxActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+}
---
+++ app/src/main/java/android/bignerdranch/com/beatbox/SoundViewModel.java
@@ -0,0 +1,21 @@
+package android.bignerdranch.com.beatbox;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+    public SoundViewModel(BeatBox beatBox) {
+        mBeatBox = beatBox;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return mSound.getName();
+    }
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
